f4d31e87ca3e7759e83e0731c696b738eb04c273
Shawn Allen <Shawn.Allen@vu.com>
2025-04-11 16:36:36 -0500

    Calculates total inventory items. Iterates through each item to display quantity and name of items.

--- displayInventory.py
+++ displayInventory.py
@@ -3,6 +3,17 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     # your code goes here
 
+    item_total = 0      # Variable for counting total of inventory items
+    
+    print("Inventory:") # Start of print statement
+
+    # Iterate through each inventory item and display the count then the item name
+    for key in inventory:
+        print(inventory[key], key)
+
+        item_total += inventory[key] # Adding each item's quantity to a total
+
+    print(f"Total number of items: {item_total}") # Displays the total number of inventory items
 
 if __name__ == "__main__":
     displayInventory(stuff)
